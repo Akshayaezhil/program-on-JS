4f433040034057b90fada372479cd6748f2e984b
Akshayaezhil <720821103009@hit.edu.in>
2024-07-15 16:17:17 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-let a=prompt("enter the number");
-for(let i=1;i<=a;i++){
-if(i%3==0 && i%5==0){
-    console.log("FizzBuzz");
-}else if(i%3==0){
-    console.log("Fizz");
-}else if(i%5==0){
-    console.log("Buzz");
-}else{
-    console.log(i);
-}}
+let arr=[1,2,3,45,5];
+function largest(arr){
+    return Math.max(...arr);
+}console.log(largest(arr));
